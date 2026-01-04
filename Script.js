6fc9ddda1c749bb073a736b3083dba0805c309b0
dea735 <kajdomqajdea@gmail.com>
2026-01-04 17:36:02 +0100

    Final commit i projektit te kozmetikes

---
+++ Script.js
@@ -0,0 +1,19 @@
+/* =========================
+   FAIRYGLOW JAVASCRIPT
+   ========================= */
+
+/* LOGIN ALERT */
+function loginAlert() {
+    alert("Welcome to FairyGlow Cosmetics 💖✨");
+}
+
+/* VALIDIM I FORMËS SË KONTAKTIT */
+function validateForm() {
+    alert("Mesazhi juaj u dërgua me sukses 🌸💌");
+    return false; // ndalon rifreskimin e faqes
+}
+
+/* EFEKT I THJESHT FAIRY (klik) */
+document.addEventListener("click", function () {
+    console.log("✨ FairyGlow magic ✨");
+});
